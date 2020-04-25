da04182a55452fa8185513f6aea2d7ca443111c8
prathikulal <64296783+prathikulal@users.noreply.github.com>
2020-04-25 20:30:53 +0530

    Update test_read_file_content.sh

--- scripts/test/test_read_file_content.sh
+++ scripts/test/test_read_file_content.sh
@@ -5,26 +5,19 @@ CONFIG="/home/pi/little-backup-box/scripts/config.cfg"
 source "$CONFIG"
 ######################################################################
 
-
+while :
+    do
 if [ -s /home/pi/rsync_dirnFiles.log ]
 then
         echo "File is not empty"
+        NoOfDir=$(grep -c "cd+++++++++" /home/pi/rsync_dirnFiles.log)
+        NoOfFiles=$(grep -c "f+++++++++" /home/pi/rsync_dirnFiles.log)
+        message="DR:$NoOfDir FL:$NoOfFiles"
+        oled r
+        oled +d "$message"
+        #rsync -avh --info=progress2 --log-file=/home/pi/rsync_dirnFiles.log --excl$
+        sudo oled s
 else
         echo "File is empty"
 fi
-
-NoOfDir=$(grep -c "cd+++++++++" /home/pi/rsync_dirnFiles.log)
-NoOfFiles=$(grep -c "f+++++++++" /home/pi/rsync_dirnFiles.log)
-oled r
-message="DR:$NoOfDir FL:$NoOfFiles"
-oled r
-oled +d "$message"
-#rsync -avh --info=progress2 --log-file=/home/pi/rsync_dirnFiles.log --excl$
-
-
-sudo oled s
-############################################################
-
-
-sync
-oled r
+done
