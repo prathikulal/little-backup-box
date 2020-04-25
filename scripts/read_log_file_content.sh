cc3424728bec08107c7c26c2283e5965e581b23f
prathikulal <64296783+prathikulal@users.noreply.github.com>
2020-04-25 20:40:43 +0530

    Create read_log_file_content.sh

---
+++ scripts/read_log_file_content.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+CONFIG="/home/pi/little-backup-box/scripts/config.cfg"
+source "$CONFIG"
+######################################################################
+
+while :
+    do
+if [ -s /home/pi/rsync_dirnFiles.log ]
+then
+        echo "log File is not empty"
+        NoOfDir=$(grep -c "cd+++++++++" /home/pi/rsync_dirnFiles.log)
+        NoOfFiles=$(grep -c "f+++++++++" /home/pi/rsync_dirnFiles.log)
+        message="DR:$NoOfDir FL:$NoOfFiles"
+        oled r
+        oled +d "$message"
+        #rsync -avh --info=progress2 --log-file=/home/pi/rsync_dirnFiles.log --excl$
+        sudo oled s
+else
+        echo "log File is empty"
+fi
+done
+#justcom
