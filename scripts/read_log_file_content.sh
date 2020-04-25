c5b8e13674daeb38fa6ca249d3041fc2cd7fda6c
prathikulal <64296783+prathikulal@users.noreply.github.com>
2020-04-25 21:44:22 +0530

    Update read_log_file_content.sh

--- scripts/read_log_file_content.sh
+++ scripts/read_log_file_content.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
-
-CONFIG="/home/pi/little-backup-box/scripts/config.cfg"
+CONFIG_DIR=$(dirname "$0")
+CONFIG="${CONFIG_DIR}/config.cfg"
 source "$CONFIG"
 ######################################################################
 
@@ -10,16 +9,19 @@ while :
 if [ -s /home/pi/rsync_dirnFiles.log ]
 then
         echo "log File is not empty"
+        sourceDnF=$(tree -a "$CARD_MOUNT_POINT"| tail -1|awk '{ print "SD:"$1" SF:"$3}')
         NoOfDir=$(grep -c "cd+++++++++" /home/pi/rsync_dirnFiles.log)
         NoOfFiles=$(grep -c "f+++++++++" /home/pi/rsync_dirnFiles.log)
-        message="DR:$NoOfDir FL:$NoOfFiles"
-        #oled r
+        message="DD:$NoOfDir DF:$NoOfFiles"
+        oled r
+        oled +a "Copying..."
+        oled +c "$sourceDnF"
         oled +d "$message"
         #rsync -avh --info=progress2 --log-file=/home/pi/rsync_dirnFiles.log --excl$
         sudo oled s
 else
         echo "log File is empty"
 fi
-#sleep 1
+sleep 1
 done
 #justcomf
