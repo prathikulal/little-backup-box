5656dca3aae196c450f79e8e4e9eb13a6621bfea
prathikulal <64296783+prathikulal@users.noreply.github.com>
2020-04-25 19:19:08 +0530

    My stabel version 1

--- scripts/card-backup.sh
+++ scripts/card-backup.sh
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################
+# my stabel version
 
 CONFIG_DIR=$(dirname "$0")
 CONFIG="${CONFIG_DIR}/config.cfg"
