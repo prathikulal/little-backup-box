96876e70fe5eefb0e37a9be3f397f32add045655
prathikulal <64296783+prathikulal@users.noreply.github.com>
2020-05-01 12:53:46 +0530

    Update test_read_file_content.sh

--- scripts/test/test_read_file_content.sh
+++ scripts/test/test_read_file_content.sh
@@ -6,16 +6,16 @@ CONFIG="/home/pi/little-backup-box/scripts/config.cfg"
 source "$CONFIG"
 ######################################################################
 
-          $copyPercentage=$(tail -n 7  /home/"$USER"/little-backup-box.log |grep %|tail -n 1|awk '{print $3}')
-        $messageA="Copying... $copyPercentage"
-          $copySize=$(tail -n 7 /home/"$USER"/little-backup-box.log |grep %|tail -n 1|awk '{print $2}')
-          $ElapseTime=$(tail -n 7 /home/"$USER"/little-backup-box.log |grep %|tail -n 1|awk '{print $5}')
-        $messageB="$copySize $ElapseTime"
-          $copySpeed=$(tail -n 7 /home/"$USER"/little-backup-box.log |grep %|tail -n 1|awk '{print $4}')
-        $messageC="Speed: $copySpeed"
-          $CPUtemp=$(vcgencmd measure_temp|awk '{split($0,a,"=");print a[2]}')
-          $CPUload=$(w|head -n 1|awk '{print $8}'|awk '{split($0,a,",");;print a[1]}')
-        $messageD="$CPUtemp CPU:$CPUload"
+          copyPercentage=$(tail -n 7  /home/"$USER"/little-backup-box.log |grep %|tail -n 1|awk '{print $3}')
+        messageA="Copying... $copyPercentage"
+          copySize=$(tail -n 7 /home/"$USER"/little-backup-box.log |grep %|tail -n 1|awk '{print $2}')
+          ElapseTime=$(tail -n 7 /home/"$USER"/little-backup-box.log |grep %|tail -n 1|awk '{print $5}')
+        messageB="$copySize $ElapseTime"
+          copySpeed=$(tail -n 7 /home/"$USER"/little-backup-box.log |grep %|tail -n 1|awk '{print $4}')
+        messageC="Speed: $copySpeed"
+          CPUtemp=$(vcgencmd measure_temp|awk '{split($0,a,"=");print a[2]}')
+          CPUload=$(w|head -n 1|awk '{print $8}'|awk '{split($0,a,",");;print a[1]}')
+        messageD="$CPUtemp CPU:$CPUload"
         oled r
         oled +a "$messageA"
         oled +b "$messageB"
